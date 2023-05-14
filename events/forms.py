fd79a801643c47d6eb4d76fb8921fc53c994bec6
Allie Sills <asills14@ivytech.edu>
2023-05-13 20:56:12 -0700

    Final commit. Added pythonanywhere as allowed host.

---
+++ events/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Event
+
+
+
+#Super user event form
+class EventFormAdmin(ModelForm):
+    class Meta:
+        model = Event
+        fields = ('game_name', 'game_date', 'game_room', 'table_host', 'game_description')
+        labels = {
+           'game_name':'Game Name' ,
+           'game_date':'Game Date YYYY-MM-DD HH:MM:SS' ,
+           'game_room':'Game Room' ,
+           'table_host':'Table Host' ,
+           'game_description':'Game Description' ,
+           
+        }
+        widgets={
+            'game_name': forms.TextInput(attrs={'class': 'form-control','placeholder':'Game Name'}),
+            'game_date': forms.TextInput(attrs={'class': 'form-control','placeholder':'YYYY-MM-DD HH:MM:SS'}),
+            'game_room': forms.Select(attrs={'class': 'form-select','placeholder':'Game Room'}),
+            'game_host': forms.Select(attrs={'class': 'form-select','placeholder':'Game Host'}),
+            'game_description': forms.Textarea(attrs={'class': 'form-control','placeholder':'Game Description'}),
+        }
+
+#Standard User Event form
+class EventFormUser(ModelForm):
+    class Meta:
+        model = Event
+        fields = ('game_name', 'game_date', 'game_room', 'game_description')
+        labels = {
+           'game_name':'Game Name' ,
+           'game_date':'Game Date' ,
+           'game_room':'Game Room' ,
+           'game_description':'Game Description' ,
+           
+        }
+        widgets={
+            'game_name': forms.TextInput(attrs={'class': 'form-control','placeholder':'Game Name'}),
+            'game_date': forms.TextInput(attrs={'class': 'form-control','placeholder':'YYYY-MM-DD HH:MM:SS'}),
+            'game_room': forms.Select(attrs={'class': 'form-select','placeholder':'Game Room'}),
+            'game_description': forms.Textarea(attrs={'class': 'form-control','placeholder':'Game Description'}),
+        }
---
+++ events/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #Use path converter of int and str to create month and year url name within the path
+    #main page
+    path('', views.index, name="index"),
+    path('<int:year>/<str:month>/', views.index, name="index"),
+
+    #games
+    path('add_game/',views.add_game,name="add-game"),
+    path('search_games/',views.search_games,name="search-games"),
+    path('games/', views.all_games, name="list-games"),
+    path('show_game/<game_id>', views.show_game, name="show-game"),
+    path('update_game/<game_id>', views.update_game, name="update-game"),
+    path('delete_game/<game_id>', views.delete_game,name='delete-game'),
+
+    #rooms
+   
+    path('rooms/', views.all_rooms, name="list-rooms"),
+    path('show_room/<room_id>', views.show_room, name="show-room"),
+   
+]
